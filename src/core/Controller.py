f91037625f75633730f2631dbc0195c386add45e
Caesar Furlanetto <caesar@alunos.utfpr.edu.br>
2023-10-05 00:54:53 -0300

    adicionando imports

--- src/core/Controller.py
+++
@@ -1,9 +0,0 @@
-from sympy import *
-import numpy as np
-import control as clt
-import matplotlib.pyplot as plt
-import math
-from control.matlab import *
-from scipy import signal
-from model import SystemControl, FunctionSystem
-
--- src/core/compensator_lead.py
+++ src/core/compensator_lead.py
@@ -1,3 +1,12 @@
+from sympy import *
+import numpy as np
+import control as clt
+import matplotlib.pyplot as plt
+import math
+from control.matlab import *
+from scipy import signal
+from model import SystemControl, FunctionSystem
+
 class CompensadorAvanco(SystemControl):
     s=clt.tf('s')
 
